3e2c45dbf0194ec9c1b87cfbd069575e47f1e815
tonadev <alejandro.garciaes@alumno.buap.mx>
2020-10-11 14:00:12 -0500

    Añade funcionalidad básica de autentificación e interactividad a través de un menú.

--- Sistema.java
+++ Sistema.java
@@ -1,4 +1,5 @@
 import java.util.Calendar;
+import java.util.Scanner;
 
 public class Sistema {
 	public static void main(String[] args) {
@@ -13,7 +14,7 @@ public class Sistema {
 					EnumTipoSucursal.MATRIZ_PRINCIPAL,
 					EnumEstado.PUEBLA,
 					true));
-		usuarios[1] = new Empleado(
+		usuarios[1] = new AdministradorEstatal(
 				"Empleado",
 				"Uno",
 				Calendar.getInstance(),
@@ -23,8 +24,8 @@ public class Sistema {
 				150000f,
 				"Gerente",
 				"1234567890",
-				"empGen",
-				"UnoEmp",
+				"test",
+				"test",
 				new Identificador(
 					EnumTipoEmpleado.ADMIN_ESTATAL,
 					true,
@@ -32,19 +33,57 @@ public class Sistema {
 					EnumTipoSucursal.MATRIZ_ESTATAL,
 					EnumEstado.JALISCO,
 					true));
+		Scanner sc = new Scanner(System.in);
+		String usuario, constraseña;
+		Usuario usuarioActual;
+		Empleado empleadoActual;
+		int op;
 
-		System.out.println(usuarios[0].getId().obtenerTipoEmpleado());
-		System.out.println(usuarios[0].getId().esEmpleadoActivo());
-		System.out.println(usuarios[0].getId().obtenerNumeroSucursal());
-		System.out.println(usuarios[0].getId().obtenerTipoSucursal());
-		System.out.println(usuarios[0].getId().obtenerEstado());
-		System.out.println(usuarios[0].getId().esTiendaActiva());
+		System.out.println("¡Bienvenido! Por favor ingrese sus credenciales para continuar");
+		System.out.print("Usuario: ");
+		usuario = sc.nextLine();
+		System.out.print("Contraseña: ");
+		constraseña = sc.nextLine();
 
-		System.out.println(usuarios[1].getId().obtenerTipoEmpleado());
-		System.out.println(usuarios[1].getId().esEmpleadoActivo());
-		System.out.println(usuarios[1].getId().obtenerNumeroSucursal());
-		System.out.println(usuarios[1].getId().obtenerTipoSucursal());
-		System.out.println(usuarios[1].getId().obtenerEstado());
-		System.out.println(usuarios[1].getId().esTiendaActiva());
+		usuarioActual = getUsuarioConCredenciales(
+				usuario,
+				constraseña,
+				usuarios);
+
+		if (usuarioActual != null) {
+			System.out.println("Credenciales válidas.");
+			empleadoActual = obtenerEmpleado(usuarioActual);
+			System.out.println(empleadoActual.obtenerMenu());
+			System.out.print("¿Qué desea realizar? ");
+			op = sc.nextInt();
+			System.out.println(empleadoActual.realizarOperacion(op));
+		} else {
+			System.out.println("No se encontro a ningún usuario con las credenciales ingresadas");
+		}
+	}
+
+	private static Usuario getUsuarioConCredenciales(String usuario,
+			String constraseña,
+			Usuario usuarios[]) {
+		for (int i = 0; i < usuarios.length; i++) {
+			if (usuarios[i].validarCredenciales(usuario, constraseña))
+				return usuarios[i];
+		}
+		return null;
+	}
+
+	private static Empleado obtenerEmpleado(Usuario u) {
+		switch (u.getId().obtenerTipoEmpleado()) {
+			case ADMIN_ESTATAL:
+				return (AdministradorEstatal) u;
+			case ADMIN_PRINCIPAL: 
+				return (AdministradorPrincipal) u;
+			case EMPLEADO_ORDINARIO:
+				return (EmpleadoOrdinario) u;
+			case OPERADOR:
+				return (Operador) u;
+			default:
+				return (Empleado) u;
+		}
 	}
 }
